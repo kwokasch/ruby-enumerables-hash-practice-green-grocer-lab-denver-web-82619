7e415a45a161a80915eac3fae20352e3512ff7d0
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-15 19:37:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,23 +24,44 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon| 
-    coupon_name = coupon[:item]
+  coupons.each do |coupon|          #=> for each coupon 
+    coupon_name = coupon[:item]     #=> set "coupon_name" variable equal to the value assoc. with the "item" key in the coupon hash
+    
       if cart.has_key?(coupon_name) && !cart.has_key?("#{coupon_name} W/COUPON") && cart[coupon_name][:count] >= coupon[:num]
+        
+        #=> if there is a coupon for an item in the car and the hash key of "#{coupon_name} W/COUPON" doesn't exist and there are still items in the cart to which the coupons can be applied
+        
         cart["#{coupon_name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num],:clearance => cart[coupon_name][:clearance],:count => coupon[:num]}
+        
+        #=> create has with "#{coupon_name} W/COUPON" as key and a set of hashes as the value; set keys within these inner hashes to match those in the cart
+        
         cart[coupon_name][:count] -= coupon[:num]
+        
+        #=> subtract count associated with coupon from the number of items in cart 
+        
       elsif cart.has_key?("#{coupon_name} W/COUPON") && cart[coupon_name][:count] >= coupon[:num]
         cart["#{coupon_name} W/COUPON"][:count] += coupon[:num]
+        #=> if the "#{coupon_name} W/COUPON" key already exists and there are still items in the cart to which coupons can be applied, 
+        
         cart[coupon_name][:count] -= coupon[:num]
+        
+        #=> subtract count associated with coupon from the number of items in cart 
+        
     end
   end
-  cart 
+  cart    #=> return cart (now updated with coupons)
 end
 
 def apply_clearance(cart)
-  cart.each do |item, price_hash|
+  cart.each do |item, price_hash|     
+    #=> for each item in cart, 
+    
     if price_hash[:clearance] == true
+      #=> if there is a clearance on the item
+      
       price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+      
+      #=> 
     end
   end
   cart
